931d0a5c583114f4868abe5e043be4f9759bfea5
iqoypresto <rfqimaulana888@gmail.com>
2026-09-05 01:59:31 +0700

    fix: add logic pada page login internal dengan pengalihan halaman jika terdapat sesi yang masih aktif untuk tidak bisa mengakses halaman login khususnya back-office

--- src/app/(auth)/internal/(auth)/login/page.tsx
+++ src/app/(auth)/internal/(auth)/login/page.tsx
@@ -1,8 +1,29 @@
-import { Paper, Stack } from "@mantine/core";
+"use client";
 
+import { Paper, Stack } from "@mantine/core";
 import { EmployeeLoginForm } from "@/components/internalAuth/EmployeeLoginForm";
+import { useAuthStore } from "@/stores/useAuthStore";
+import { useRouter } from "next/navigation";
+import { useEffect } from "react";
+
+const backOfficeRole = ["OUTLET_ADMIN", "SUPER_ADMIN"];
 
 export default function EmployeeLoginPage() {
+  const router = useRouter();
+  const user = useAuthStore((state) => state.user);
+
+  useEffect(() => {
+    if (!user || !backOfficeRole.includes(user.role)) {
+      return;
+    }
+
+    router.replace(
+      user.role === "OUTLET_ADMIN"
+        ? "/internal/outlet-admin/dashboard"
+        : "/internal/super-admin/dashboard"
+    );
+  }, [user, router]);
+
   return (
     <Paper maw={420} mx="auto" mt={64} p={32} radius="md" withBorder>
       <Stack gap="md">
